ceecf56908cae5fde68f1cce94dde69f61fc2c5e
Vishaal Upot <84678878+vishaalupot@users.noreply.github.com>
2024-09-16 07:44:09 +0400

    addeds files

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import './App.css'
 import Corporate from './components/Corporate/Corporate';
 import Culture from './components/Company/Culture/Culture';
 import Nav from './components/Nav/Nav';
+import AboutAes from './components/Company/AboutAesthetix/AboutAes';
+import CallSupport from './components/Company/CallSupport/Support';
+import Location from './components/Company/Locations/Location';
 
 function App() {
   return (
     <>
       <Nav />
+      {/* <AboutAes /> */}
       <Corporate />
-      <Culture />
+      {/* <Location /> */}
+      {/* <CallSupport /> */}
+      {/* <Culture /> */}
     </>
   )
 }
