2e7208d45b8e17757c5347f9c29b80eab404b59a
NiranjanNR <kidkool8@gmail.com>
2024-09-18 22:46:29 +0530

    merge changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,13 @@ import './App.css'
 import Corporate from './components/Corporate/Corporate';
 import Culture from './components/Company/Culture/Culture';
 import Nav from './components/Nav/Nav';
-import AboutAes from './components/Company/AboutAesthetix/AboutAes';
-import CallSupport from './components/Company/CallSupport/Support';
-import Location from './components/Company/Locations/Location';
-import Av from './components/Solutions/AudioVisual/Av';
 
 function App() {
   return (
     <>
       <Nav />
-      {/* <AboutAes /> */}
-      <Av />
-      {/* <Corporate /> */}
-      {/* <Location /> */}
-      {/* <CallSupport /> */}
-      {/* <Culture /> */}
+      <Corporate />
+      <Culture />
     </>
   )
 }
