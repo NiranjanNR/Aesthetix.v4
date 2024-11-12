651fff99504785d863e9c41fcd9cd054a45e76bf
NiranjanNR <kidkool8@gmail.com>
2024-11-12 12:48:50 +0530

    location fix

--- src/components/Company/Locations/Location.tsx
+++ src/components/Company/Locations/Location.tsx
@@ -147,15 +147,13 @@ AL Farazdaq ST, Ad Dhubat
             </div>
 
             {/* Support Section with Waves */}
-            <div className='relative'>
-                {/* Top Waves */}
+            {/* <div className='relative'>
                 <div className="waves top-waves">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
                         <path fill="rgb(243, 232, 253)" d="M0,160L48,170.7C96,181,192,203,288,192C384,181,480,139,576,122.7C672,107,768,117,864,138.7C960,160,1056,192,1152,197.3C1248,203,1344,181,1392,170.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
                     </svg>
                 </div>
 
-                {/* Support Design */}
                 <div className='supportDesign text-center py-10'>
                     <div className='primaryH text-2xl sm:text-3xl font-bold'>NEED SUPPORT?</div>
                     <div className='primaryP text-base sm:text-lg py-4'>
@@ -170,13 +168,12 @@ AL Farazdaq ST, Ad Dhubat
                     </button>
                 </div>
 
-                {/* Bottom Waves */}
                 <div className="waves bottom-waves">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
                         <path fill="rgb(243, 232, 253)" d="M0,160L48,149.3C96,139,192,117,288,128C384,139,480,181,576,197.3C672,213,768,203,864,181.3C960,160,1056,128,1152,122.7C1248,117,1344,139,1392,149.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
                     </svg>
                 </div>
-            </div>
+            </div> */}
 
             {/* PSNI Alliance Section */}
             <div>
